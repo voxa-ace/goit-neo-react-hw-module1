ab9a1cbc8bae2b6c362f3393082f3b0abe8461a7
Volodymyr Kashnikov <vkashnikov19@gmail.com>
2024-07-28 18:20:29 +0300

    add markup for friend list

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem';
+import styles from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles.friendList}>
+      {friends.map(friend => (
+        <li key={friend.id} className={styles.friendListItem}>
+          <FriendListItem 
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+    id: PropTypes.number.isRequired
+  })).isRequired
+};
+
+export default FriendList;
